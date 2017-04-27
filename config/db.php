a373ffafb4a941bb471b31889de43a1bd0e4a569
Evgeniy Averyanov <root@evgeniy-webmaster.pro>
2017-04-27 13:58:16 +0000

    создание модели Doc

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=localhost;dbname=test',
+    'username' => 'test',
+    'password' => 'test',
     'charset' => 'utf8',
 ];
